038e62ade6f4084e0c70e46000007788e392a85d
Nathan N <Nitharios@users.noreply.github.com>
2017-10-24 14:14:57 -1000

    Basic setup

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 /* jshint esversion:6 */
 const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./models');
 const PORT = process.env.PORT || 8888;
 
 const app = express();
 
 app.listen(PORT, () => {
+  db.sequelize.sync({ force: true });
   console.log(`Server listening on ${PORT}`);
 });

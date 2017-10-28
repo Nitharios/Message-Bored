4a4adbb899f71c83fd3cc838202e5583dbe5e6c1
Nathan N <Nitharios@users.noreply.github.com>
2017-10-28 09:19:14 -1000

    Able to show/hide login/logout/register based on user status

--- public/scripts/controllers/home.controller.js
+++ public/scripts/controllers/home.controller.js
@@ -1,12 +1,7 @@
 angular.module('boredApp')
 .controller('NavBarController', ['$scope', '$window', '$location', function($scope, $window, $location) {
 
-  $scope.user = { loggedIn : false };
-
-  console.log(typeof $window.localStorage.isLoggedIn);
-
-  if (localStorage.getItem($window.localStorage.isLoggedIn) === true) {
-    console.log('here');
-    $scope.user.loggedIn = true;
-  }
+  $scope.isUserLoggedIn = function() {
+    return localStorage.getItem('isLoggedIn') === 'true';
+  };
 }]);
